4c33308d7197e980024eadef00b9b7f279823b20
talhaansari04 <talhaansari61@gmail.com>
2024-12-08 23:35:02 +0530

    First

---
+++ src/main/java/com/igse/dto/WalletInfoDTO.java
@@ -0,0 +1,17 @@
+package com.igse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WalletInfoDTO {
+    private String walletId;
+    private String customerId;
+    private Double totalBalance;
+    private LocalDate creationDate;
+}
